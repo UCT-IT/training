1fb87d91b317adfeacda8b9c992ddb5efa1cb6d2
Md. Jobaye Hossain <jobayerh.uctitbd@gmail.com>
2023-12-11 13:23:52 +0600

    changed

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,63 +1,23 @@
-
 import logo from "../../../../public/images/nav/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
-import { FaBars, FaTimes } from "react-icons/fa";
 import { strings } from "../../../assets/locales/locales";
-import { navbarState } from "../navbarState/NavbarState";
-
+import NavbarState from "../navbarState/NavbarState";
 
 const Navbar = () => {
   const localeText = strings.nav;
-  // const [isMenuOpen, setMenuOpen] = useState(false);
-  // const handleToggleMenu = () => {
-  //   setMenuOpen(!isMenuOpen);
-  // };
-  // const handleMobileMenuKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
-  //   if (e.key === "Enter") {
-  //     setMenuOpen(!isMenuOpen);
-  //   }
-  // };
-  const {handleMobileMenuKeyDown,isMenuOpen,handleToggleMenu} = navbarState()
   return (
     <div className="shadow-lg py-7 lg:px-0 z-50 sticky top-0 bg-white">
       <div className="container px-5 mx-auto flex items-center justify-between text-[#0F437F] relative">
         <Link href="/">
           <Image src={logo} alt="logo" />
         </Link>
-        <div onKeyDown={handleMobileMenuKeyDown} tabIndex={0} className="md:hidden">
-          <button
-            onClick={handleToggleMenu}
-            className={`text-white z-50 bg-[#0F437F] hover:bg-[#F9A07C] transition-all h-9 w-9 rounded-full grid place-items-center focus:outline-none text-[17px]`}
-          >
-            {isMenuOpen ? <FaTimes /> : <FaBars />}
-          </button>
-        </div>
+        <NavbarState />
         <ul
           className={`hidden z-50 md:flex md:items-center gap-10 font-bold text-[17px]`}
         >
           {localeText.map((item) => (
-            <li
-              key={item.text}
-              className="hover:text-[#F9A07C] transition-all"
-            >
-              <Link href={item.url}>{item.text}</Link>
-            </li>
-          ))}
-        </ul>
-        {/* mobile menu */}
-        <ul
-          className={`${
-            isMenuOpen
-              ? "translate-x-0 delay-100"
-              : "translate-x-full opacity-10"
-          } md:hidden fixed top-[85px] left-0 h-screen z-50 w-full bg-white  text-[#0F437F] font-bold text-[18px] transition-transform ease-in-out duration-500 transform space-y-4 pt-3 px-14 lg:px-0`}
-        >
-          {localeText.map((item) => (
-            <li
-              key={item.text}
-              className="hover:text-[#F9A07C] transition-all"
-            >
+            <li key={item.text} className="hover:text-[#F9A07C] transition-all">
               <Link href={item.url}>{item.text}</Link>
             </li>
           ))}
