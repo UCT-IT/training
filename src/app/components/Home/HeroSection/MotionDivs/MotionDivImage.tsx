2be0350566f3fc2bd08dfea3931fee4b4133976f
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2023-12-11 17:55:45 +0600

    react intersection observer added

--- src/app/components/Home/HeroSection/MotionDivs/MotionDivImage.tsx
+++ src/app/components/Home/HeroSection/MotionDivs/MotionDivImage.tsx
@@ -1,12 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import heroImg from "../../../../../../public/images/Home/HeroSection/hero.jpeg";
 import { strings } from "@/app/assets/locales/locales";
+import { InView } from "react-intersection-observer";
 
 const MotionDivImage = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+
   const localeText = strings.home.heroSection;
   const containerVariants = {
     visible: {
@@ -28,37 +30,33 @@ const MotionDivImage = () => {
     },
   };
 
-  const handleScroll = () => {
-    const scrollPosition = window.scrollY;
-    setIsScrolled(scrollPosition > 100);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
     <React.Fragment>
-      <motion.div
-        initial="visible"
-        animate={isScrolled ? "visible" : "hidden"}
-        variants={containerVariants}
-        className={`mt-5 lg:mt-0`}
+      <InView
+        as="div"
+        threshold={0.7}
+        initialInView={false}
+        onChange={(inView) => setIsScrolled(inView)}
       >
         <motion.div
-          initial={{ translateY: 500, opacity: 0, rotate: 10 }}
-          animate={{ translateY: 0, opacity: 1, rotate: 0 }}
-          transition={{ duration: 0.5, ease: "linear" }}
+          initial="visible"
+          animate={isScrolled ? "visible" : "hidden"}
+          variants={containerVariants}
+          className={`mt-5 lg:mt-0`}
         >
-          <Image
-            className={`max-w-full rounded-3xl`}
-            src={heroImg}
-            alt={localeText.altText}
-          />
+          <motion.div
+            initial={{ translateY: 500, opacity: 0, rotate: 10 }}
+            animate={{ translateY: 0, opacity: 1, rotate: 0 }}
+            transition={{ duration: 0.5, ease: "linear" }}
+          >
+            <Image
+              className={`max-w-full rounded-3xl`}
+              src={heroImg}
+              alt={localeText.altText}
+            />
+          </motion.div>
         </motion.div>
-      </motion.div>
+      </InView>
     </React.Fragment>
   );
 };
