50004a247f46a42aa8618a4b2a1f9e2dbeff02c2
Md. Jobaye Hossain <jobayerh.uctitbd@gmail.com>
2023-12-17 14:18:38 +0600

    completed history section

--- src/app/components/about/history/History.tsx
+++ src/app/components/about/history/History.tsx
@@ -5,7 +5,7 @@ import { strings } from "@/app/assets/locales/locales";
 const History = () => {
   const localeText = strings.about.history
   return (
-    <div className="py-40 mb-[500px] bg-[#F9FAFB]">
+    <div className="py-40 bg-[#F9FAFB]">
       <div className="responsive-container grid grid-cols-1 md:grid-cols-2 gap-5 font-kumbh">
         <div className="relative md:sticky md:top-24 self-start">
           <h1 className="text-[38px] font-bold font-newsreader text-secondary">
