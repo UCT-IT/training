722530e2e1fca9fe2d2c2c36c0c3b955fcb17122
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2023-12-21 12:22:48 +0600

    updated

--- src/app/components/Home/ContactUs/ContactUs.tsx
+++ src/app/components/Home/ContactUs/ContactUs.tsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
   const localeText = strings.home.contactUsSection;
 
   return (
-    <div className="responsive-container bg-white rounded-2xl shadow-lg text-secondary my-28 relative overflow-clip">
+    <div className="responsive-container bg-white rounded-2xl shadow-lg text-textColor my-28 relative overflow-clip">
       <div className="bg-info w-[100px] h-[100px] rounded-ss-full absolute right-0 bottom-0"></div>
       <div className="bg-warning w-[100px] h-[100px] rounded-ee-full absolute left-0 top-0"></div>
       <div className="flex flex-col lg:flex-row justify-center items-center relative">
@@ -19,7 +19,7 @@ const ContactUs = () => {
           </p>
 
           <hr className="w-full" />
-          <div className="text-secondary">
+          <div>
             <p className="font-bold mt-5">{localeText.addressTitle}</p>
             <p>
               <span className="font-semibold">{localeText.officeBd}</span>{" "}
@@ -41,25 +41,25 @@ const ContactUs = () => {
             </p>
           </div>
         </div>
-        <div className="text-secondary py-16 px-5 text-center lg:text-left relative z-20">
+        <div className="py-16 px-5 text-center lg:text-left relative z-20">
           <h2 className="text-3xl mb-3 font-semibold ps-2">
             {localeText.sendMessageTitle}
           </h2>
-          <form className="px-2 text-textColor">
+          <form className="px-2">
             <input
-              className="border-[2px] border-secondary focus:border-black bg-gray-300 placeholder-black  w-full mb-5 outline-none rounded-xl p-2"
+              className="border-[2px] border-gray-400 focus:border-black bg-gray-300 placeholder-black  w-full mb-5 outline-none rounded-xl p-2"
               required
               type="text"
               placeholder={localeText.placeholderText.name}
             />
             <input
-              className=" border-[2px] border-secondary focus:border-black bg-gray-300 placeholder-black w-full mb-5 outline-none rounded-xl p-2"
+              className=" border-[2px] border-gray-400 focus:border-black bg-gray-300 placeholder-black w-full mb-5 outline-none rounded-xl p-2"
               required
               type="email"
               placeholder={localeText.placeholderText.email}
             />
             <textarea
-              className="border-[2px] border-secondary focus:border-black bg-gray-300 placeholder-black w-full mb-5 outline-none rounded-xl p-2"
+              className="border-[2px] border-gray-400 focus:border-black bg-gray-300 placeholder-black w-full mb-5 outline-none rounded-xl p-2"
               required
               cols={30}
               rows={10}
