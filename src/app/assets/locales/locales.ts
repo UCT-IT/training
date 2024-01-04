3871f69aaf2a5ba357dcf62c5eb7271025b56bab
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2024-01-04 14:04:36 +0600

    updated

--- src/app/assets/locales/locales.ts
+++ src/app/assets/locales/locales.ts
@@ -216,9 +216,9 @@ export let strings = new LocalizedStrings({
         eiusmod tempor incididunt ut labore et dolore magna.`,
       },
       allCourses: {
-        Category: {text: "all", url: "/pages/courses"},
+        Category: { text: "all", url: "/pages/courses" },
         title: "All Courses",
-      }
+      },
       featuredCourse: {
         heading: `Featured Course`,
         altTextThumbnail: `Course Thumbnail Image`,
