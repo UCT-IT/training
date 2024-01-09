081e97f37b84a08c06d14f9a70aca670a6ec87d3
Rasheduluctit <rasheduli.uctitbd@gmail.com>
2024-01-09 12:27:45 +0600

    updated

--- src/app/components/About/HeroSection/HeroSection.tsx
+++ src/app/components/About/HeroSection/HeroSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import MotionDivCircleTwo from "./MotionDivs/MotionDivCircleTwo";
-import MotionDivTexts from "./MotionDivs/MotionDivTexts";
 import MotionDivCircleOne from "./MotionDivs/MotionDivCircleOne";
 
 const HeroSection = () => {
@@ -9,7 +8,6 @@ const HeroSection = () => {
       <div className="relative z-10 overflow-hidden py-[200px]">
         <MotionDivCircleOne />
         <MotionDivCircleTwo />
-        <MotionDivTexts />
       </div>
     </React.Fragment>
   );
